d93de821e7619f6f465f277bd905b6781d31ebd0
surajkumar <suraj.kumar@digital.cabinet-office.gov.uk>
2023-12-19 19:45:00 +0000

    Spotless

--- src/main/java/io/github/surajkumar/concurrency/promise/Status.java
+++ src/main/java/io/github/surajkumar/concurrency/promise/Status.java
@@ -1,23 +1,16 @@
 package io.github.surajkumar.concurrency.promise;
 
-/**
- * The Status enum represents the different statuses that a Promise can have.
- */
+/** The Status enum represents the different statuses that a Promise can have. */
 public enum Status {
-    /**
-     * Represents the status of a promise that has not been started yet.
-     */
+    /** Represents the status of a promise that has not been started yet. */
     NOT_STARTED,
-    /**
-     * Represents the status of a promise that has successfully completed its execution.
-     */
+    /** Represents the status of a promise that has successfully completed its execution. */
     FINISHED,
-    /**
-     * The RUNNING constant represents the status of a promise that is currently running.
-     */
+    /** The RUNNING constant represents the status of a promise that is currently running. */
     RUNNING,
     /**
-     * The ERROR constant represents the status of a promise that encountered an error while running.
+     * The ERROR constant represents the status of a promise that encountered an error while
+     * running.
      */
     ERROR
 }
