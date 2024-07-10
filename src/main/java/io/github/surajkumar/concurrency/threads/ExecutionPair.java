eebd70a19e2f1d6472cd6376f3e6cea9b42aa543
Suraj Kumar <sk96.uk@gmail.com>
2024-07-10 12:36:49 +0100

    Convert ExecutionPair into a Record

--- src/main/java/io/github/surajkumar/concurrency/threads/ExecutionPair.java
+++ src/main/java/io/github/surajkumar/concurrency/threads/ExecutionPair.java
@@ -6,37 +6,4 @@ import io.github.surajkumar.concurrency.promise.Promise;
  * The ExecutionPair class represents a pair of a Promise and its corresponding ExecutionSettings.
  * It provides methods to retrieve the Promise and ExecutionSettings.
  */
-public class ExecutionPair {
-    private final Promise<?> promise;
-    private final ExecutionSettings executionSettings;
-
-    /**
-     * The ExecutionPair class represents a pair of a Promise and its corresponding
-     * ExecutionSettings. It provides methods to retrieve the Promise and ExecutionSettings.
-     *
-     * @param promise The promise being executed
-     * @param executionSettings The execution settings
-     */
-    public ExecutionPair(Promise<?> promise, ExecutionSettings executionSettings) {
-        this.promise = promise;
-        this.executionSettings = executionSettings;
-    }
-
-    /**
-     * Retrieves the Promise associated with this ExecutionPair.
-     *
-     * @return the Promise associated with this ExecutionPair
-     */
-    public Promise<?> getPromise() {
-        return promise;
-    }
-
-    /**
-     * Retrieves the ExecutionSettings associated with this ExecutionPair.
-     *
-     * @return the ExecutionSettings associated with this ExecutionPair
-     */
-    public ExecutionSettings getExecutionSettings() {
-        return executionSettings;
-    }
-}
+public record ExecutionPair(Promise<?> promise, ExecutionSettings executionSettings) {}
